fff8bc04a52a2c92093937e612b8373c691f1610
9kongpob <k.kunchanuan@hotmail.com>
2019-11-21 09:36:25 +0700

    Complete Coding v.0.0.1

--- Form1.cs
+++ Form1.cs
@@ -67,6 +67,58 @@ namespace BasicPart12_WindowFormApplication
         private void btn_save_Click(object sender, EventArgs e)
         {
 
+            Clipboard.SetImage(pb_qrcode.Image);
+
+            //Image File;
+            //SaveFileDialog f = new SaveFileDialog();
+            //f.Filter = "Image files (*.jpg, *.png) | *.jpg; *.png";
+
+            //if (f.ShowDialog() == DialogResult.OK)
+            //{
+                //File = Image.FromFile(f.FileName);
+                //pb_qrcode.Image = File;
+                //pb_qrcode.Image.Save();
+
+                //pb_qrcode.Image.Save(specific_folder + "\\" + f.SafeFileName);
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Filter = "JPEG Image|*.jpg|PNG Image (.png)|*.png|Bitmap Image|*.bmp|Gif Image|*.gif|Tiff Image (.tiff)|*.tiff|Wmf Image (.wmf)|*.wmf";
+                saveFileDialog1.Title = "Save as";
+                saveFileDialog1.ShowDialog();
+
+                if (saveFileDialog1.FileName != "")
+                {
+                    System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+
+                    switch (saveFileDialog1.FilterIndex)
+                    {
+                        case 1:
+                            this.pb_qrcode.Image.Save(fs,
+                              System.Drawing.Imaging.ImageFormat.Jpeg);
+                            break;
+
+                        case 2:
+                            this.pb_qrcode.Image.Save(fs,
+                              System.Drawing.Imaging.ImageFormat.Bmp);
+                            break;
+
+                        case 3:
+                            this.pb_qrcode.Image.Save(fs,
+                              System.Drawing.Imaging.ImageFormat.Gif);
+                            break;
+                    }
+
+                    fs.Close();
+                //}
+            }
+
+            /*            OpenFileDialog opFile = new OpenFileDialog();
+                        opFile.Title = "Save As";
+                        opFile.Filter = "jpg files (*.jpg)|*.jpg|All files (*.*)|*.*";*/
+
+
+            //saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+
+
             /*Bitmap bmp = new Bitmap(pb_qrcode.Width, pb_qrcode.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
             Graphics g = Graphics.FromImage(bmp);
             g.FillRectangle(Brushes.White, 0, 0, Width, Height);
